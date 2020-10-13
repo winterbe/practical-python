1bad5dda459c67b3666bfcd5778a9aae9b94b62e
Benjamin Winterberg <winterbe@gmail.com>
2020-10-13 10:37:52 +0200

    Exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,22 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+while principal > 0:
+    month = month + 1
+    extra = 1000
+    if month > 12:
+        extra = 0
+    principal = principal * (1 + rate / 12) - payment - extra
+    total_paid = total_paid + payment + extra
+
+total_paid_rounded = round(total_paid * 100) / 100
+total_paid_formatted = '{:,}'.format(total_paid_rounded)
+
+print('Total paid', total_paid_formatted, 'over', month, 'months')

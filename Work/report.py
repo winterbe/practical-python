5876092e02323a84436191197590c71dc8cf1c6e
Benjamin Winterberg <winterbe@gmail.com>
2020-10-14 13:49:14 +0200

    Print report table

--- Work/report.py
+++ Work/report.py
@@ -35,11 +35,32 @@ def read_prices(filename):
     return prices
 
 
+def make_report(portfolio, prices):
+    rows = []
+    for p in portfolio:
+        name = p["name"]
+        share = p["share"]
+        price = p["price"]
+        current_price = prices[name]
+        change = current_price - price
+        rows.append((name, share, current_price, change))
+    return rows
+
+
 portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices("Data/prices.csv")
 
-total_cost = 0.0
-for p in portfolio:
-    price = p["share"] * prices[p["name"]]
-    total_cost = total_cost + price
-print(total_cost)
+# total_cost = 0.0
+# for p in portfolio:
+#     price = p["share"] * prices[p["name"]]
+#     total_cost = total_cost + price
+# print(total_cost)
+
+
+report = make_report(portfolio, prices)
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+line = '-' * 10
+print('%10s %10s %10s %10s' % (line, line, line, line))
+for name, shares, price, change in report:
+    print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')

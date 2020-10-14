0e3f9a8b622260407e28e79bf8a6c6d01d3f34c4
Benjamin Winterberg <winterbe@gmail.com>
2020-10-14 09:09:44 +0200

    Functions

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
-f = open("Data/portfolio.csv", "rt")
-next(f)
-result = 0.0
-for line in f:
-    row = line.split(",")
-    share = int(row[1])
-    price = float(row[2])
-    result = result + (share * price)
-print(result)
-f.close()
+
+def portfolio_cost(filename):
+    f = open(filename, "rt")
+    try:
+        next(f)
+        cost = 0.0
+        for line in f:
+            try:
+                row = line.split(",")
+                share = int(row[1])
+                price = float(row[2])
+                cost = cost + (share * price)
+            except ValueError:
+                print("Could not handle row:", line, end="")
+        return cost
+    finally:
+        f.close()
+
+
+print(portfolio_cost("Data/portfolio.csv"))
+print(portfolio_cost("Data/missing.csv"))

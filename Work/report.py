bf13a10899d29a66a811c2b81c08666abf2a1feb
Benjamin Winterberg <winterbe@gmail.com>
2020-10-14 13:05:13 +0200

    Tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,23 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)  # skip header
+        for row in rows:
+            name = row[0]
+            share = int(row[1])
+            price = float(row[2])
+            holding = (name, share, price)
+            portfolio.append(holding)
+    return portfolio
+
+
+portfolio = read_portfolio('Data/portfolio.csv')
+print(portfolio)

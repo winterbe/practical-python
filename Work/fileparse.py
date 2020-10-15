f3ef6533144c46b67bd90ce5d3258af801d3acf1
Benjamin Winterberg <winterbe@gmail.com>
2020-10-15 08:56:04 +0200

    fileparser

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,57 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+from pprint import pprint
+
+
+def parse_csv(filename, select=None, types=None, has_headers=False, delimiter=','):
+    """
+    Parse a CSV file into a list of records
+    """
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
+
+        # Read the file headers
+        if has_headers:
+            headers = next(rows)
+        else:
+            headers = None
+
+        # If a column selector was given, find indices of the specified columns.
+        # Also narrow the set of headers used for resulting dictionaries
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
+        records = []
+        for row in rows:
+            if not row:  # Skip rows with no data
+                continue
+
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [row[index] for index in indices]
+
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+
+            records.append(record)
+
+    return records
+
+
+def test():
+    portfolio = parse_csv('Data/portfolio.dat', delimiter=' ')
+    pprint(portfolio)
+
+
+test()
